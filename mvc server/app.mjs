d0a345b8dcc20d980c11abf76f8beba42c48a344
harisnaseer24 <harisnaseer258@gmail.com>
2025-09-22 19:05:53 +0500

    Mongo db connected + product Schema created

--- mvc server/app.mjs
+++ mvc server/app.mjs
@@ -1,5 +1,7 @@
 import express from 'express'
 import productRouter from './routes/productRoutes.mjs'
+import mongoose from 'mongoose';
+
 
 const app = express()
 const port = 3000
@@ -10,6 +12,19 @@ app.get('/', (req, res) => {
 })
 
 app.use("/product",productRouter)
+
+
+
+//MongoDb Connectivity;
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://harisTest:oSWuM1rGBXq4zBxX@cluster0.cnwrfjk.mongodb.net/2307F2');
+  console.log("MongoDB Connected Successfully")
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 
 app.listen(port, () => {
